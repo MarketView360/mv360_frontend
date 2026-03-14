b8b45c9d6ea17495a0050c9eb447a8746270d1b3
Robinkumar J <robin246j@gmail.com>
2026-03-14 12:00:51 +0530

    microsoft clarity analysis addon script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { ReactNode } from "react";
 import { Inter, Lexend_Mega } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 import "./globals.css";
 import { cn } from "@/lib/utils";
@@ -68,6 +69,19 @@ export default function RootLayout({
         </ThemeProvider>
         <Analytics />
         <SpeedInsights />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "vvhult0bm8");
+            `,
+          }}
+        />
       </body>
     </html>
   );
